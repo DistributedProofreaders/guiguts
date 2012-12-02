83c22a76239332db2f2bd33b95c6b0da4cfee0e4
Hanne <hannne@users.sourceforge.net>
2012-12-02 18:45:54 +0100

    modifying About popup

--- lib/Guiguts/HelpMenu.pm
+++ lib/Guiguts/HelpMenu.pm
@@ -34,8 +34,8 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-Guiguts 1.0 prepared by Hunter Monroe and many others.
 Original guiguts written by Stephen Schulze.
+Later versions prepared by Hunter Monroe and many others.
 Partially based on the Gedi editor - Gregs editor.
 Redistributable on the same terms as Perl.
 EOM
