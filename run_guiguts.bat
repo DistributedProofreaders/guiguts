c2e3ed87b1c372bb5ebb2e6a6dc3d63d177b8880
Casey Peel <cpeel@users.noreply.github.com>
2020-06-07 10:50:16 -0700

    Use Strawberry Perl for the Windows interpreter
    Rather than bundle our own version of Perl for Windows, instruct
    users to use Strawberry Perl instead.

--- run_guiguts.bat
+++ run_guiguts.bat
@@ -1,10 +1,21 @@
 @echo off
 
-cd %~dp0
+REM Save original path to restore later. This prevents the path from being
+REM updated at every run.
+set OLDPATH=%PATH%
 
-set PATH=%cd%\perl;%cd%\perl\lib;%cd%\python27;%cd%\python27\scripts;%cd%\tools\kindlegen;%cd%\tools\tidy;%PATH%
+REM Prefix Strawberry Perl to the path
+set PATH=C:\Strawberry\c\bin;C:\Strawberry\perl\site\bin;C:\Strawberry\perl\bin;%PATH%
+
+REM Add older bundled perl & python directories to the path at the end
+set PATH=%PATH%;%cd%\perl;%cd%\perl\lib;%cd%\python27;%cd%\python27\scripts;
+
+REM Prefix tooling directories
+set PATH=%cd%\tools\kindlegen;%cd%\tools\tidy;%PATH%
 
 rem set ASPELL_CONF=conf-dir c:/guiguts/tools/aspell/bin
 
-
 perl guiguts.pl %1
+
+REM Restore original path
+set PATH=%OLDPATH%
