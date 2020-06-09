f8a997ec65357282ba3bce664e39b76a3004a347
Casey Peel <cpeel@users.noreply.github.com>
2020-06-08 18:08:17 -0700

    Remove deprecated release-building artifacts

--- make.bat
+++
@@ -1,9 +0,0 @@
-erase g*.zip
-7z a -x!.* -x!setting.rc -x!header.txt -x!*.bat -x!gg.ico -x!tools -x!tests -x!perl -x!Python27 -x!*.zip -x!Win*.* -r guiguts-1.0.19.zip *.* lib\Tk\Toolbar\tkIcons 
-REM TODO: bundle bookloupe
-7z a -x!.* -x!*.exe -r guiguts-1.0.19.zip tools\gutcheck tools\jeebies
-
-REM other.zip contains perl, Python27 and tools
-REM TODO: remove Python27 (also includes old version of epubmaker)
-copy other.zip guiguts-win-1.0.19.zip
-7z a -x!.* -x!setting.rc -x!header.txt -x!make.bat -x!gg.ico -x!*.zip -x!tools -x!wordlist -x!scannos -x!perl -x!Python27 -r guiguts-win-1.0.19.zip *.* lib\Tk\Toolbar\tkIcons
--- src/run_guiguts.bat
+++ src/run_guiguts.bat
@@ -7,8 +7,9 @@ set OLDPATH=%PATH%
 REM Prefix Strawberry Perl to the path
 set PATH=C:\Strawberry\c\bin;C:\Strawberry\perl\site\bin;C:\Strawberry\perl\bin;%PATH%
 
-REM Add older bundled perl & python directories to the path at the end
-set PATH=%PATH%;%cd%\perl;%cd%\perl\lib;%cd%\python27;%cd%\python27\scripts;
+REM Add older bundled perl directory to the path at the end as a fall-back
+REM for users who want to try and use it instead of Strawberry Perl.
+set PATH=%PATH%;%cd%\perl;%cd%\perl\lib
 
 REM Prefix tooling directories
 set PATH=%cd%\tools\kindlegen;%cd%\tools\tidy;%PATH%
