4759fd3fe6e38bd3b8f2309730f9ca98bfe7873e
Casey Peel <cpeel@users.noreply.github.com>
2020-06-10 17:14:38 -0700

    Package bookloupe

---
+++ tools/bookloupe/README.md
@@ -0,0 +1,16 @@
+# bookloupe
+
+bookloupe is a fork of guiguts that has been updated to support UTF-8.
+
+http://www.juiblex.co.uk/pgdp/bookloupe/
+
+## Packaging
+
+Guiguts bundles a Windows binary for Windows releases.
+
+MacOS users can install bookloupe via Homebrew with:
+```
+brew install bookloupe
+```
+
+Linux users will need to download and build it manually.
---
+++ tools/bookloupe/package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+OS=$1
+DEST=$2
+
+# Exit on any failure
+set -e
+
+DEST=$DEST/bookloupe
+mkdir $DEST
+cp README.md $DEST
+
+if [[ $OS == "win" ]]; then
+    URL=http://www.juiblex.co.uk/pgdp/bookloupe/bookloupe-2.0-win32.zip
+    curl -L -o bookloupe.zip $URL
+    unzip bookloupe.zip -d $DEST
+    rm -rf bookloupe.zip
+fi
+
+# bookloupe is available via Homebrew on MacOS
+# Linux users will need to download and build it manually.
