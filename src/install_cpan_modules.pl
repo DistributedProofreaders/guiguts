710adfb8e463baa1a7abde7136f43c40d3065bf0
Casey Peel <cpeel@users.noreply.github.com>
2020-06-11 13:08:54 -0700

    Include optional perl modules
    Several perl modules are technically optional but are very useful.
    In particular, without LWP::Protocol::https we can't check for updates.

--- src/install_cpan_modules.pl
+++ src/install_cpan_modules.pl
@@ -1,9 +1,15 @@
 #!/usr/bin/env perl
 
 my @modules = (
+    # Required modules
     "LWP::UserAgent",
     "Tk",
     "Tk::ToolBar",
+    # Optional but recommended modules
+    "Text::LevenshteinXS",
+    "File::Which",
+    "Image::Size",
+    "LWP::Protocol::https",  # needed for update checking
 );
 
 foreach my $module (@modules) {
