bf196514e524c35554aa8acfea4fb730957c7441
Casey Peel <cpeel@users.noreply.github.com>
2020-06-10 17:01:47 -0700

    Create new release packaging framework
    Add framework to package tools upon build.

---
+++ tools/package-tools.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script packages up all of the relevant tools in this directory
+# by delegating to other package.sh scripts.
+
+OS=$1
+DEST=$2
+
+if [[ -z $OS || -z $DEST ]]; then
+    echo "Usage: $0 OS DEST_DIR"
+    exit 1
+fi
+
+# Exit on any failure
+set -e
+
+BASE_DIR=$(pwd)
+TOOLS_DIR=$(dirname $0)
+
+PACKAGES=$(find $TOOLS_DIR -name package.sh)
+for package in $PACKAGES; do
+    package=$(dirname $package)
+    echo "--------------------------------------------------------------------"
+    echo Packaging $package
+    cd $BASE_DIR/$package
+    ./package.sh $OS $DEST
+done
