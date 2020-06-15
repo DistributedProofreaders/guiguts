1c803ff3d1ca2076adad307db587970428d95b7e
Nigel <windymilla@users.noreply.github.com>
2020-06-15 21:51:20 +0100

    Document use of batch file argument variables
    Document use of %~dp0 to get dir name where
    GG is installed, and %~f1 to get full pathname of
    file to be edited.

--- src/run_guiguts.bat
+++ src/run_guiguts.bat
@@ -1,5 +1,10 @@
 @echo off
 
+REM Get the Guiguts install directory as the directory containing this batch file
+set GG_DIR=%~dp0
+REM Get full pathname of edit file from first argument to batch file
+set ED_FILE=%~f1
+
 REM Save original path to restore later. This prevents the path from being
 REM updated at every run.
 set OLDPATH=%PATH%
@@ -9,14 +14,14 @@ set PATH=C:\Strawberry\c\bin;C:\Strawberry\perl\site\bin;C:\Strawberry\perl\bin;
 
 REM Add older bundled perl directory to the path at the end as a fall-back
 REM for users who want to try and use it instead of Strawberry Perl.
-set PATH=%PATH%;%~dp0perl;%~dp0perl\lib
+set PATH=%PATH%;%GG_DIR%perl;%GG_DIR%perl\lib
 
 REM Prefix tooling directories
-set PATH=%~dp0tools\kindlegen;%~dp0tools\tidy;%PATH%
+set PATH=%GG_DIR%tools\kindlegen;%GG_DIR%tools\tidy;%PATH%
 
 REM Find guiguts.pl in same folder as this batch file
 REM Use full pathname of file to be edited
-perl %~dp0guiguts.pl %~f1
+perl %GG_DIR%guiguts.pl %ED_FILE%
 
 REM Restore original path
 set PATH=%OLDPATH%
