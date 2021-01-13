DistributedProofreaders/guiguts

commit bde0971fbb682e95bdfa29760c5ddc95788ee8d5

Don't attempt to write uninitialized label info to the bin file
Historically, uninitialized errors were suppressed with a pragma, removed by #241

Check explicitly if label has been set before outputting label information.

src/lib/Guiguts/FileMenu.pm
@@ -299,12 +299,19 @@ sub _bin_save {
             if ( $page eq "Pg" ) {
                 next;
             }
+
+            # output page and offset
             print $fh " '$page' => {";
             print $fh "'offset' => '$::pagenumbers{$page}{offset}', ";
-            print $fh "'label' => '$::pagenumbers{$page}{label}', ";
-            print $fh "'style' => '$::pagenumbers{$page}{style}', ";
-            print $fh "'action' => '$::pagenumbers{$page}{action}', ";
-            print $fh "'base' => '$::pagenumbers{$page}{base}'},\n";
+
+            # if labels have been set up, output label information too
+            if ( $::pagenumbers{$page}{label} ) {
+                print $fh "'label' => '$::pagenumbers{$page}{label}', ";
+                print $fh "'style' => '$::pagenumbers{$page}{style}', ";
+                print $fh "'action' => '$::pagenumbers{$page}{action}', ";
+                print $fh "'base' => '$::pagenumbers{$page}{base}'";
+            }
+            print $fh "},\n";
         }
         print $fh ");\n\n";
         foreach ( keys %::operationshash ) {

commit 683973c1bc3df30d196319883f7dc48181fc6f51

Save page labels in bin file more safely
Bug introduced by #432 - trying to avoid outputting label info if undefined.
Fix was too aggressive, meaning required information was not output.
Changed to mimic what used to be output, but without accessing undefined
variables

src/lib/Guiguts/FileMenu.pm
@@ -306,12 +306,10 @@ sub _bin_save {
             print $fh "'offset' => '$::pagenumbers{$page}{offset}', ";
 
             # if labels have been set up, output label information too
-            if ( $::pagenumbers{$page}{label} ) {
-                print $fh "'label' => '$::pagenumbers{$page}{label}', ";
-                print $fh "'style' => '$::pagenumbers{$page}{style}', ";
-                print $fh "'action' => '$::pagenumbers{$page}{action}', ";
-                print $fh "'base' => '$::pagenumbers{$page}{base}'";
-            }
+            print $fh "'label' => '" .  ( $::pagenumbers{$page}{label}  || "" ) . "', ";
+            print $fh "'style' => '" .  ( $::pagenumbers{$page}{style}  || "" ) . "', ";
+            print $fh "'action' => '" . ( $::pagenumbers{$page}{action} || "" ) . "', ";
+            print $fh "'base' => '" .   ( $::pagenumbers{$page}{base}   || "" ) . "'";
             print $fh "},\n";
         }
         print $fh ");\n\n";