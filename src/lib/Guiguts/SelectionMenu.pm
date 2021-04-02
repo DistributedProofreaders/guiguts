DistributedProofreaders/guiguts

commit 14870ba1b20a37de9a8d0cd3b81f3e47d08f6fcc

Speed up sorting of words in Word Frequency dialog
Processing of all the additional Latin accented characters caused a large performance
penalty. Fixed by only using the slow conversion on words that need it.

Fixes #666

src/lib/Guiguts/Utilities.pm
@@ -491,9 +491,15 @@ sub escape_regexmetacharacters {
 
 sub deaccentsort {
     my $phrase = shift;
-    return $phrase unless ( $phrase =~ /[$::convertcharssinglesearch]/ );
-    eval "\$phrase =~ tr/$::convertcharssinglesearch/$::convertcharssinglereplace/";
-    $phrase =~ s/([$::convertcharsmultisearch])/$::convertcharssort{$1}/g;
+    if ( $phrase =~ /[$::convertcharssinglesearch]/ ) {    # do we need the slow tr?
+        eval
+          "\$phrase =~ tr/$::convertlatinsinglesearch$::convertcharssinglesearch/$::convertlatinsinglereplace$::convertcharssinglereplace/";
+    } elsif ( $phrase =~ /[$::convertlatinsinglesearch]/ ) {    # do we need tr at all?
+        eval "\$phrase =~ tr/$::convertlatinsinglesearch/$::convertlatinsinglereplace/";
+    } elsif ( $phrase !~ /[$::convertcharsmultisearch]/ ) {     # Contains no chars we need to treat specially
+        return $phrase;
+    }
+    $phrase =~ s/([$::convertcharsmultisearch])/$::convertcharssort{$1}/g;    # Handle user-defined sort character substitutions
     return $phrase;
 }
 
@@ -527,20 +533,24 @@ sub readlabels {
     }
 
     # Prepare the strings to be used for deaccenting:
+    # - Latin1 versions used for speed if more exotic accents not needed in deaccentsort
     # - Single-char-search and single-char-replace to be used in tr/single-char-search/single-char-replace/ in deaccentsort
     # - Multi-char-search to be used together with convertcharssort in s/// in deaccentsort
     # - Display-multi-char-search to be used with convertcharsdisplay in s/// in deaccentdisplay
     $::convertcharsdisplaysearch = join( '', keys %::convertcharsdisplay );
     $::convertcharsmultisearch   = join( '', keys %::convertcharssort );
 
-    # Contains the accented characters from the Latin Unicode blocks
+    # Latin-1 only
+    $::convertlatinsinglesearch =
+      "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏ" . "ĞÑÒÓÔÕÖØÙÚÛÜİŞß" . "àáâãäåæçèéêëìíîï" . "ğñòóôõöøùúûüışÿ";
+    $::convertlatinsinglereplace =
+      "AAAAAAACEEEEIIII" . "DNOOOOOOUUUUYTs" . "aaaaaaaceeeeiiii" . "dnoooooouuuuyty";
+
+    # Contains the accented characters from the Latin Unicode blocks (slower to use)
     $::convertcharssinglesearch =
 
-      # Latin-1 Supplement
-      "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏ" . "ĞÑÒÓÔÕÖØÙÚÛÜİŞß" . "àáâãäåæçèéêëìíîï" . "ğñòóôõöøùúûüışÿ"
-
       # Latin Extended A
-      . "\x{100}\x{101}\x{102}\x{103}\x{104}\x{105}\x{106}\x{107}\x{108}\x{109}\x{10a}\x{10b}\x{10c}\x{10d}\x{10e}\x{10f}"
+      "\x{100}\x{101}\x{102}\x{103}\x{104}\x{105}\x{106}\x{107}\x{108}\x{109}\x{10a}\x{10b}\x{10c}\x{10d}\x{10e}\x{10f}"
       . "\x{110}\x{111}\x{112}\x{113}\x{114}\x{115}\x{116}\x{117}\x{118}\x{119}\x{11a}\x{11b}\x{11c}\x{11d}\x{11e}\x{11f}"
       . "\x{120}\x{121}\x{122}\x{123}\x{124}\x{125}\x{126}\x{127}\x{128}\x{129}\x{12a}\x{12b}\x{12c}\x{12d}\x{12e}\x{12f}"
       . "\x{130}\x{131}\x{134}\x{135}\x{136}\x{137}\x{139}\x{13a}\x{13b}\x{13c}\x{13d}\x{13e}\x{13f}"
@@ -585,11 +595,8 @@ sub readlabels {
     # Contains the non-accented forms of the accented characters above
     $::convertcharssinglereplace =
 
-      # Latin-1 Supplement
-      "AAAAAAACEEEEIIII" . "DNOOOOOOUUUUYTs" . "aaaaaaaceeeeiiii" . "dnoooooouuuuyty"
-
       # Latin Extended A
-      . "AaAaAaCcCcCcCcDd"
+      "AaAaAaCcCcCcCcDd"
       . "DdEeEeEeEeEeGgGg"
       . "GgGgHhHhIiIiIiIi"
       . "IiJjKkLlLlLlL"
@@ -630,11 +637,19 @@ sub readlabels {
       . "OoOoUuUuUuUuUuUu"
       . "UuYyYyYyYy";
 
+    # Remove characters from the default sort strings if they exist in the user's language .rc file
     my @chararray = keys %::convertcharssort;
     for ( my $i = 0 ; $i < @chararray ; $i++ ) {
-        my $index = index( $::convertcharssinglesearch, $chararray[$i] );
-        substr $::convertcharssinglesearch,  $index, 1, '';
-        substr $::convertcharssinglereplace, $index, 1, '';
+        my $index = index( $::convertlatinsinglesearch, $chararray[$i] );
+        if ( $index >= 0 ) {
+            substr $::convertlatinsinglesearch,  $index, 1, '';
+            substr $::convertlatinsinglereplace, $index, 1, '';
+        }
+        $index = index( $::convertcharssinglesearch, $chararray[$i] );
+        if ( $index >= 0 ) {
+            substr $::convertcharssinglesearch,  $index, 1, '';
+            substr $::convertcharssinglereplace, $index, 1, '';
+        }
     }
 }
 

commit 53b149588030c82b9e58f3055bdf22873088be1b

Fix errors revealed by test suite
1. Bug introduced into wrapping - occasionally adds a blank line after a block, due
to error in extending end of block to end of line - should go to start of next line,
but only if not already at start of line. Caused by #665
2. Bug introduced into de-accenting for display when separating out Latin1
characters into their own array for sorting speed up. Caused by #667.
3. Baseline test files needed updating now that error display dialog has its own
count of active errors, so error list doesn't need it. Caused by #597

src/lib/Guiguts/SelectionMenu.pm
@@ -191,9 +191,10 @@ sub selectrewrap {
 
         $thisblockend = $end unless $thisblockend;    # if no end found, finish at end of selection
 
-        # Always start/end at beginning/end of lines
+        # Always start/end at beginning of lines
         $thisblockstart = $textwindow->index( $thisblockstart . ' linestart' );
-        $thisblockend   = $textwindow->index( $thisblockend . ' lineend' );
+        my ( $ll, $cc ) = split( /\./, $textwindow->index($thisblockend) );
+        $thisblockend = $textwindow->index( $thisblockend . ' +1l linestart' ) unless $cc == 0;
 
         $selection = $textwindow->get( $thisblockstart, $thisblockend ) if $thisblockend;    # get the paragraph of text
         unless ($selection) {

src/lib/Guiguts/Utilities.pm
@@ -506,13 +506,14 @@ sub deaccentsort {
 
 sub deaccentdisplay {
     my $phrase = shift;
-    return $phrase unless ( $phrase =~ /[$::convertcharssinglesearch]/ );
+    return $phrase unless ( $phrase =~ /[$::convertlatinsinglesearch$::convertcharssinglesearch]/ );
 
     # first convert the characters specified by the language
     $phrase =~ s/([$::convertcharsdisplaysearch])/$::convertcharsdisplay{$1}/g;
 
     # then convert anything that hasn't been converted already
-    eval "\$phrase =~ tr/$::convertcharssinglesearch/$::convertcharssinglereplace/";
+    eval
+      "\$phrase =~ tr/$::convertlatinsinglesearch$::convertcharssinglesearch/$::convertlatinsinglereplace$::convertcharssinglereplace/";
     return $phrase;
 }
 