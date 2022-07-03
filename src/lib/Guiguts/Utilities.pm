4c374b403d5244219aa8331c75a85798d4c8e4d1
Dan Lowe <dan@tangledhelix.com>
2022-07-03 01:53:45 -0400

    Set kindlegen path to an absolute path
    The default path to kindlegen on startup (without setting.rc) is
    "Applications/Kindle Previewer 3.app/Contents/lib/fc/bin/kindlegen"
    
    Without a leading '/' or './' or '../' Guiguts will consider this
    a relative path. Unless the user happens to start guiguts while
    in the root directory, this will never work.
    
    This patch adjusts the default value for $kindleviewercommand
    to include the leading '/'

--- src/lib/Guiguts/Utilities.pm
+++ src/lib/Guiguts/Utilities.pm
@@ -1128,8 +1128,8 @@ sub initialize {
         $::kindlegencommand = ::setdefaultpath(
             $::kindlegencommand,
             ::catfile(
-                'Applications', 'Kindle Previewer 3.app',
-                'Contents',     'lib', 'fc', 'bin', 'kindlegen'
+                '/Applications', 'Kindle Previewer 3.app',
+                'Contents', 'lib', 'fc', 'bin', 'kindlegen'
             )
         );
     } else {
