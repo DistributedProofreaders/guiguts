6e8071338554ae394daa937c662bc286148d0933
Nigel <windymilla@users.noreply.github.com>
2024-01-02 15:23:01 +0000

    Update ebookmaker, CSS validator & Changelog

--- tools/W3C/package.sh
+++ tools/W3C/package.sh
@@ -11,7 +11,7 @@ mkdir $DEST
 cp README.md $DEST
 
 # css-validator.jar is the same for all systems
-VERSION=20220105
+VERSION=20231124
 curl -L -o $DEST/css-validator.jar https://github.com/w3c/css-validator/releases/download/cssval-$VERSION/css-validator.jar
 
 # vnu.jar is the same for all systems
